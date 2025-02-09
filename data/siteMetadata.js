5b27d568f5dde13f289d74d5fa2be40c8cbf63d2
Bogdan Mocanu <bogdan.mocanu@gmail.com>
2025-02-09 23:52:23 +0200

    Update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Bogdan Mocanu personal blog',
+  author: 'Bogdan Mocanu',
+  headerTitle: 'IT blog bits',
+  description: 'A blog created by Bogdan Mocanu',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
